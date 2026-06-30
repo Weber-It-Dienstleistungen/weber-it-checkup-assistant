21967d31c7fddd916918fb40d4ba1cb716d71a15
Weber-It-Dienstleistungen <fabian.weber@weber-it-nec.de>
2026-06-30 15:56:06 +0200

    Introduce MVVM foundation

---
+++ src/WeberIT.Checkup.App/WeberIT.Checkup.App/ViewModels/MainViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WeberIT.Checkup.App.ViewModels;
+
+public class MainViewModel
+{
+    public string ApplicationTitle { get; } = "Weber IT Checkup Assistent";
+
+    public string ApplicationSubtitle { get; } =
+        "Professioneller Windows-Checkup für Kunden, Geräte und Dokumentation.";
+}
