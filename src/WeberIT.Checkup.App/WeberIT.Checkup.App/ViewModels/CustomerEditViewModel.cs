8075bc5685245b2834d3d58754505aaec6ac57ff
Weber-It-Dienstleistungen <fabian.weber@weber-it-nec.de>
2026-07-01 15:14:49 +0200

    Add customer edit view model

---
+++ src/WeberIT.Checkup.App/WeberIT.Checkup.App/ViewModels/CustomerEditViewModel.cs
@@ -0,0 +1,28 @@
+﻿using WeberIT.Checkup.App.Models;
+
+namespace WeberIT.Checkup.App.ViewModels;
+
+public class CustomerEditViewModel : BaseViewModel
+{
+    private Customer _customer;
+
+    public Customer Customer
+    {
+        get => _customer;
+        set
+        {
+            _customer = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public CustomerEditViewModel()
+    {
+        Customer = new Customer();
+    }
+
+    public CustomerEditViewModel(Customer customer)
+    {
+        Customer = customer;
+    }
+}
