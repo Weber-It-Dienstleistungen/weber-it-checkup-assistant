d355c797a67844e37fdff82a0b5449044b5436c9
Weber-It-Dienstleistungen <fabian.weber@weber-it-nec.de>
2026-07-01 08:40:36 +0200

    Add CurrentView navigation placeholder

--- src/WeberIT.Checkup.App/WeberIT.Checkup.App/ViewModels/MainViewModel.cs
+++ src/WeberIT.Checkup.App/WeberIT.Checkup.App/ViewModels/MainViewModel.cs
@@ -1,9 +1,28 @@
-﻿namespace WeberIT.Checkup.App.ViewModels;
+﻿using WeberIT.Checkup.App.Views.Pages;
 
-public class MainViewModel
+namespace WeberIT.Checkup.App.ViewModels;
+
+public class MainViewModel : BaseViewModel
 {
     public string ApplicationTitle { get; } = "Weber IT Checkup Assistent";
 
     public string ApplicationSubtitle { get; } =
         "Professioneller Windows-Checkup für Kunden, Geräte und Dokumentation.";
+
+    private object? _currentView;
+
+    public object? CurrentView
+    {
+        get => _currentView;
+        set
+        {
+            _currentView = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public MainViewModel()
+    {
+        CurrentView = new DashboardView();
+    }
 }
