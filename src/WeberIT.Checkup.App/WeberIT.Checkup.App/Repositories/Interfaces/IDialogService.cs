65ed53e13363173255a4eb9f1f4a1104107a4baa
Weber-It-Dienstleistungen <fabian.weber@weber-it-nec.de>
2026-07-01 15:35:16 +0200

    Add dialog service interface

---
+++ src/WeberIT.Checkup.App/WeberIT.Checkup.App/Repositories/Interfaces/IDialogService.cs
@@ -0,0 +1,8 @@
+﻿using WeberIT.Checkup.App.Models;
+
+namespace WeberIT.Checkup.App.Services.Interfaces;
+
+public interface IDialogService
+{
+    bool? ShowCustomerEditDialog(Customer customer);
+}
---
+++ src/WeberIT.Checkup.App/WeberIT.Checkup.App/Views/Dialogs/CustomerEditDialog.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WeberIT.Checkup.App.Views.Dialogs
+{
+    /// <summary>
+    /// Interaktionslogik für CustomerEditDialog.xaml
+    /// </summary>
+    public partial class CustomerEditDialog : Window
+    {
+        public CustomerEditDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
