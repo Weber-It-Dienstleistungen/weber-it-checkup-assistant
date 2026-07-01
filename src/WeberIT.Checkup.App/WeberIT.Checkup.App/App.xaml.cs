d34af138c44faf5f47e534a4509e978d93016269
Weber-It-Dienstleistungen <fabian.weber@weber-it-nec.de>
2026-07-01 08:10:03 +0200

    Introduce Generic Host and dependency injection

--- src/WeberIT.Checkup.App/WeberIT.Checkup.App/App.xaml.cs
+++ src/WeberIT.Checkup.App/WeberIT.Checkup.App/App.xaml.cs
@@ -1,14 +1,40 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using WeberIT.Checkup.App.ViewModels;
 
-namespace WeberIT.Checkup.App
+namespace WeberIT.Checkup.App;
+
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    private readonly IHost _host;
+
+    public App()
     {
+        _host = Host.CreateDefaultBuilder()
+            .ConfigureServices((context, services) =>
+            {
+                services.AddSingleton<MainViewModel>();
+                services.AddSingleton<MainWindow>();
+            })
+            .Build();
     }
 
+    protected override async void OnStartup(StartupEventArgs e)
+    {
+        await _host.StartAsync();
+
+        var mainWindow = _host.Services.GetRequiredService<MainWindow>();
+        mainWindow.Show();
+
+        base.OnStartup(e);
+    }
+
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        await _host.StopAsync();
+        _host.Dispose();
+
+        base.OnExit(e);
+    }
 }
--- src/WeberIT.Checkup.App/WeberIT.Checkup.App/MainWindow.xaml.cs
+++ src/WeberIT.Checkup.App/WeberIT.Checkup.App/MainWindow.xaml.cs
@@ -5,10 +5,10 @@ namespace WeberIT.Checkup.App;
 
 public partial class MainWindow : Window
 {
-    public MainWindow()
+    public MainWindow(MainViewModel viewModel)
     {
         InitializeComponent();
 
-        DataContext = new MainViewModel();
+        DataContext = viewModel;
     }
 }
