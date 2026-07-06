0da465e6078846e6a645990091c2b9413843b9ae
Weber-It-Dienstleistungen <fabian.weber@weber-it-nec.de>
2026-07-06 15:00:45 +0200

    Add device domain model

---
+++ src/WeberIT.Checkup.App/WeberIT.Checkup.App/Models/Devices.cs
@@ -0,0 +1,22 @@
+﻿namespace WeberIT.Checkup.App.Models;
+
+public class Device
+{
+    public string DeviceNumber { get; set; } = string.Empty;
+
+    public string CustomerNumber { get; set; } = string.Empty;
+
+    public string Name { get; set; } = string.Empty;
+
+    public string DeviceType { get; set; } = string.Empty;
+
+    public string Manufacturer { get; set; } = string.Empty;
+
+    public string Model { get; set; } = string.Empty;
+
+    public string SerialNumber { get; set; } = string.Empty;
+
+    public string OperatingSystem { get; set; } = string.Empty;
+
+    public string Notes { get; set; } = string.Empty;
+}
