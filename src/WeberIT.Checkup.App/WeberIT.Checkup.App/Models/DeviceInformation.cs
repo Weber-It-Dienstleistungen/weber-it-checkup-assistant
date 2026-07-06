f4f131df8e7dba10d84080855b91f37e9981d7b0
Weber-It-Dienstleistungen <fabian.weber@weber-it-nec.de>
2026-07-06 15:04:02 +0200

    Add device information model

---
+++ src/WeberIT.Checkup.App/WeberIT.Checkup.App/Models/DeviceInformation.cs
@@ -0,0 +1,14 @@
+﻿namespace WeberIT.Checkup.App.Models;
+
+public class DeviceInformation
+{
+    public string Name { get; set; } = string.Empty;
+
+    public string DeviceType { get; set; } = string.Empty;
+
+    public string Manufacturer { get; set; } = string.Empty;
+
+    public string Model { get; set; } = string.Empty;
+
+    public string SerialNumber { get; set; } = string.Empty;
+}
