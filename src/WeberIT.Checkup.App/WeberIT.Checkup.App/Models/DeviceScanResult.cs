95ba8d774cbd9c4da7a0f9b9c59ba842e5d108c9
Weber-It-Dienstleistungen <fabian.weber@weber-it-nec.de>
2026-07-06 15:20:48 +0200

    Add device scan result model

---
+++ src/WeberIT.Checkup.App/WeberIT.Checkup.App/Models/DeviceScanResult.cs
@@ -0,0 +1,8 @@
+﻿namespace WeberIT.Checkup.App.Models;
+
+public class DeviceScanResult
+{
+    public DateTime ScanDate { get; set; } = DateTime.Now;
+
+    public DeviceInformation DeviceInformation { get; set; } = new();
+}
