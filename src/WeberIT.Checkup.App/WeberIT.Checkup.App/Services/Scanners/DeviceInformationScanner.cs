0bc335ae4566d36b09683d5618f6abb0d7df5a13
Weber-It-Dienstleistungen <fabian.weber@weber-it-nec.de>
2026-07-06 15:56:41 +0200

    Add device information scanner

---
+++ src/WeberIT.Checkup.App/WeberIT.Checkup.App/Services/Scanners/DeviceInformationScanner.cs
@@ -0,0 +1,29 @@
+﻿using WeberIT.Checkup.App.Models;
+using WeberIT.Checkup.App.Services.Interfaces;
+
+namespace WeberIT.Checkup.App.Services.Scanners;
+
+public class DeviceInformationScanner : IDeviceInformationScanner
+{
+    private readonly IWindowsInformationProvider _windowsInformationProvider;
+
+    public DeviceInformationScanner(
+        IWindowsInformationProvider windowsInformationProvider)
+    {
+        _windowsInformationProvider = windowsInformationProvider;
+    }
+
+    public ScanResult<DeviceInformation> Scan()
+    {
+        var deviceInformation = new DeviceInformation
+        {
+            Name = _windowsInformationProvider.GetComputerName()
+        };
+
+        return new ScanResult<DeviceInformation>
+        {
+            IsSuccessful = true,
+            Data = deviceInformation
+        };
+    }
+}
