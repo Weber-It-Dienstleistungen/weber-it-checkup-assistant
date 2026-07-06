db247db006e8d1945abc561c006b3cf9a05b0462
Weber-It-Dienstleistungen <fabian.weber@weber-it-nec.de>
2026-07-06 15:25:47 +0200

    Add device scanner implementation

---
+++ src/WeberIT.Checkup.App/WeberIT.Checkup.App/Services/Scanners/DeviceScanner.cs
@@ -0,0 +1,15 @@
+﻿using WeberIT.Checkup.App.Models;
+using WeberIT.Checkup.App.Services.Interfaces;
+
+namespace WeberIT.Checkup.App.Services.Scanners;
+
+public class DeviceScanner : IDeviceScanner
+{
+    public DeviceScanResult Scan()
+    {
+        return new DeviceScanResult
+        {
+            ScanDate = DateTime.Now
+        };
+    }
+}
