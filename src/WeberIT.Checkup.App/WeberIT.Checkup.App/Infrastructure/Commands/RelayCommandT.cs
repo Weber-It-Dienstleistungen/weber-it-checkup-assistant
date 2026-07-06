41ffec108b426d74b8e077f61f8d61cf003a986f
Weber-It-Dienstleistungen <fabian.weber@weber-it-nec.de>
2026-07-06 12:50:08 +0200

    Add generic RelayCommand implementation

---
+++ src/WeberIT.Checkup.App/WeberIT.Checkup.App/Infrastructure/Commands/RelayCommandT.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows.Input;
+
+namespace WeberIT.Checkup.App.Infrastructure.Commands;
+
+public class RelayCommand<T> : ICommand
+{
+    private readonly Action<T?> _execute;
+    private readonly Predicate<T?>? _canExecute;
+
+    public RelayCommand(Action<T?> execute, Predicate<T?>? canExecute = null)
+    {
+        _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        _canExecute = canExecute;
+    }
+
+    public event EventHandler? CanExecuteChanged;
+
+    public bool CanExecute(object? parameter)
+    {
+        return _canExecute?.Invoke(ConvertParameter(parameter)) ?? true;
+    }
+
+    public void Execute(object? parameter)
+    {
+        _execute(ConvertParameter(parameter));
+    }
+
+    public void RaiseCanExecuteChanged()
+    {
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    private static T? ConvertParameter(object? parameter)
+    {
+        if (parameter is null)
+        {
+            return default;
+        }
+
+        if (parameter is T value)
+        {
+            return value;
+        }
+
+        return default;
+    }
+}
