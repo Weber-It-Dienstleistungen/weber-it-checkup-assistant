694201ee26718201b9f5a1363f80608c014f7a8b
Weber-It-Dienstleistungen <fabian.weber@weber-it-nec.de>
2026-07-06 12:53:48 +0200

    Add base class for view model validation

---
+++ src/WeberIT.Checkup.App/WeberIT.Checkup.App/ViewModels/ValidatableViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace WeberIT.Checkup.App.ViewModels;
+
+public abstract class ValidatableViewModel : BaseViewModel, INotifyDataErrorInfo
+{
+    public bool HasErrors => false;
+
+    public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
+
+    public System.Collections.IEnumerable GetErrors(string? propertyName)
+    {
+        return Enumerable.Empty<string>();
+    }
+
+    protected void RaiseErrorsChanged(string propertyName)
+    {
+        ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+    }
+}
