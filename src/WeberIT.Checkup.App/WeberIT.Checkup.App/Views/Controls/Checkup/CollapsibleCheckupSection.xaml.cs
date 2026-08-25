b827d5c5fee66ae2250f9d3d14330c551d6d1be9
Weber-It-Dienstleistungen <fabian.weber@weber-it-nec.de>
2026-08-25 08:27:08 +0200

    feat(ui): mache Checkupbereiche einklappbar

---
+++ src/WeberIT.Checkup.App/WeberIT.Checkup.App/Views/Controls/Checkup/CollapsibleCheckupSection.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WeberIT.Checkup.App.Views.Controls.Checkup;
+
+public partial class CollapsibleCheckupSection :
+    UserControl
+{
+    public static readonly DependencyProperty
+        TitleProperty =
+            DependencyProperty.Register(
+                nameof(Title),
+                typeof(string),
+                typeof(CollapsibleCheckupSection),
+                new PropertyMetadata(
+                    string.Empty));
+
+    public static readonly DependencyProperty
+        SectionContentProperty =
+            DependencyProperty.Register(
+                nameof(SectionContent),
+                typeof(object),
+                typeof(CollapsibleCheckupSection),
+                new PropertyMetadata(
+                    null));
+
+    public static readonly DependencyProperty
+        IsExpandedProperty =
+            DependencyProperty.Register(
+                nameof(IsExpanded),
+                typeof(bool),
+                typeof(CollapsibleCheckupSection),
+                new PropertyMetadata(
+                    false));
+
+    public CollapsibleCheckupSection()
+    {
+        InitializeComponent();
+    }
+
+    public string Title
+    {
+        get =>
+            (string)GetValue(
+                TitleProperty);
+
+        set =>
+            SetValue(
+                TitleProperty,
+                value);
+    }
+
+    public object? SectionContent
+    {
+        get =>
+            GetValue(
+                SectionContentProperty);
+
+        set =>
+            SetValue(
+                SectionContentProperty,
+                value);
+    }
+
+    public bool IsExpanded
+    {
+        get =>
+            (bool)GetValue(
+                IsExpandedProperty);
+
+        set =>
+            SetValue(
+                IsExpandedProperty,
+                value);
+    }
+}
